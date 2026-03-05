3f0ccad00c42a8b16a1fe68e55a3a95c5225abf1
paulowebphp <paulowebphp@gmail.com>
2026-03-05 11:29:23 -0300

    debug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Define o caminho base como relativo para funcionar em qualquer subdiretório
+  base: './',
   // Garante que rotas como /20260302_01 funcionem no modo dev
   appType: 'spa',
 })
